3de3671f18b308d6e7668779151c70a1d4c31d05
Гюнай Басриев <giunai622@gmail.com>
2026-02-17 15:14:18 +0200

    Промяна на изгледа за началната страница.

--- IntercityTransportManagementSystem/IntercityTransportManagementSystem/Program.cs
+++ IntercityTransportManagementSystem/IntercityTransportManagementSystem/Program.cs
@@ -1,5 +1,7 @@
 using IntercityTransportManagementSystem.Data;
 using IntercityTransportManagementSystem.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
 namespace IntercityTransportManagementSystem
 {
@@ -11,8 +13,18 @@ namespace IntercityTransportManagementSystem
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<IntercityTransportManagementSystemDatabaseContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                    options.AccessDeniedPath = "/Account/AccessDenied";
+                });
 
             var app = builder.Build();
+            var env = builder.Environment;
 
             using (var scope = app.Services.CreateScope())
             {
@@ -20,19 +32,30 @@ namespace IntercityTransportManagementSystem
                 DbInitializer.SeedAdmin(context);
             }
 
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
